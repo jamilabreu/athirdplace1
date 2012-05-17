6afabe841d7c1328f40ad2df9416ac7437255ef2
Jamil Abreu <abreu.jamil@gmail.com>
2012-05-17 19:20:51 -0400

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,17 @@ Athirdplace::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.action_mailer.default_url_options = { :host => 'athirdplace.com' }
+  config.action_mailer.perform_deliveries = true 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => "mail.athirdplace.com",
+  :authentication => :login,
+  :user_name => "conversations@athirdplace.com",
+  :password => "helloeveryone11",
+  }
 end
